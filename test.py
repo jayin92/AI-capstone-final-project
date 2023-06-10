96417d2311f905ddb22d585b05b6c6cf1ee78ed9
Jay Lee <jayin920805@gmail.com>
2023-06-11 00:46:45 +0800

    Add ACO method

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import tsplib95
-problem = tsplib95.load('tsplib95/archives/problems/tsp/berlin52.tsp')
+problem = tsplib95.load('tsp_graph/berlin52.tsp')
 
 for i in problem.get_nodes():
     print(problem.as_name_dict()['node_coords'][i])
